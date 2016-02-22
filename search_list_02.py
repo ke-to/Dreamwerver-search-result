66afc81ccc475f34a7b69ef604ee2623577b6ded
ke-to <kuronokei0606@gmail.com>
2016-02-22 11:54:26 +0900

    first commit

---
+++ search_list_02.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*- 
+
+import xml.dom.minidom,os,sys,glob,re
+
+li = []
+fileList = glob.glob("*.xml")
+print(fileList)
+
+for xmlFile in fileList:
+    if __name__ == "__main__":
+        #print(xmlFile)
+        dom = xml.dom.minidom.parse(xmlFile) #Your xml File
+        displaystr = dom.getElementsByTagName("mm_displaystr")
+        for i,url in enumerate(dom.getElementsByTagName("mm_file")): #Parameter name to get
+            li.append([url.firstChild.data,displaystr[i].firstChild.data])
+
+    list_uniq = sorted(li)
+    print (list_uniq)
+
+    fileName = '%s_02.txt' % xmlFile
+
+    f = open(fileName, 'w')
+    f.write("")
+
+    for i in list_uniq: 
+        f = open(fileName, 'a')
+        f.writelines(i[0].replace('\\', '/'))
+        f.writelines("\t")
+        f.writelines(i[1].encode('utf-8'))
+        f.writelines('\n')
+
+    f.close
